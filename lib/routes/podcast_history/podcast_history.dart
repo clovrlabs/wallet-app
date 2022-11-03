0df566bd3eebac3e65c553e330d0226239c6c0f3
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-11-03 14:25:10 +0300

    Replace accentTextTheme with ThemeExtension counterparts

--- lib/routes/podcast_history/podcast_history.dart
+++ lib/routes/podcast_history/podcast_history.dart
@@ -336,7 +336,7 @@ class PodcastHistoryPageState extends State<PodcastHistoryPage> {
                             AutoSizeText(
                               AppLocalizations.of(context)
                                   .podcast_history_empty_text,
-                              style: themeData.accentTextTheme.bodyText2
+                              style: themeData.statusTextStyle
                                   .copyWith(color: Colors.white),
                               textAlign: TextAlign.center,
                               minFontSize: MinFontSize(context).minFontSize,
